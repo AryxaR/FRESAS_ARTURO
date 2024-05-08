e83e21a6076eca89a8340f1ad4c7dbbf870a9e1a
NicoAvella09 <140751616+NicoAvella09@users.noreply.github.com>
2024-05-08 07:43:23 -0500

    Update catalogo.js

--- resource/js/catalogo.js
+++ resource/js/catalogo.js
@@ -31,9 +31,7 @@ function ready() {
     button.addEventListener("click", agregarAlCarritoClicked);
   }
 
-  document
-    .getElementsByClassName("btn-pagar")[0]
-    .addEventListener("click", pagarClicked);
+ 
 }
 
 function mostrarCarrito() {
@@ -180,6 +178,7 @@ function agregarItemAlCarrito(titulo, precio, imagenSrc) {
     `;
   item.innerHTML = itemCarritoContenido;
   itemsCarrito.append(item);
+  console.log(itemsCarrito)
 
   item
     .getElementsByClassName("btn-eliminar")[0]
@@ -247,52 +246,4 @@ function hacerVisibleCarrito() {
     xhr.send(JSON.stringify(itemsFactura));
 }
 
-
-(function () {
-  const listElements = document.querySelectorAll(".menu__item--show");
-  const list = document.querySelector(".menu__links");
-  const menu = document.querySelector(".menu__hamburguer");
-
-  const addClick = () => {
-    listElements.forEach((element) => {
-      element.addEventListener("click", () => {
-        let subMenu = element.children[1];
-        let height = 0;
-        element.classList.toggle("menu__item--active");
-
-        if (subMenu.clientHeight === 0) {
-          height = subMenu.scrollHeight;
-        }
-
-        subMenu.style.height = `${height}px`;
-      });
-    });
-  };
-
-  const deleteStyleHeight = () => {
-    listElements.forEach((element) => {
-      if (element.children[1].getAttribute("style")) {
-        element.children[1].removeAttribute("style");
-        element.classList.remove("menu__item--active");
-      }
-    });
-  };
-
-  window.addEventListener("resize", () => {
-    if (window.innerWidth > 800) {
-      deleteStyleHeight();
-      if (list.classList.contains("menu__links--show"))
-        list.classList.remove("menu__links--show");
-    } else {
-      addClick();
-    }
-  });
-
-  if (window.innerWidth <= 800) {
-    addClick();
-  }
-
-  menu.addEventListener("click", () =>
-    list.classList.toggle("menu__links--show")
-  );
-})();
+;
