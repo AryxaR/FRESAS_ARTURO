f29824d2b5caa7653d2a3b3825d652ea812aab01
NicoAvella09 <140751616+NicoAvella09@users.noreply.github.com>
2024-05-29 23:01:24 -0500

    intento fallido pdf pedidos

--- model/interfaz_admin/Pedidos.php
+++ model/interfaz_admin/Pedidos.php
@@ -221,7 +221,6 @@
 
                             <?php if ($pedido['estado'] == 'activo') : ?>
                                 <a href="detalle_pedido.php?id_factura=<?php echo $pedido['id_factura']; ?>" class="btn btn-primary">Ver Detalles</a>
-                                <a href="generar_pdf.php?id=<?php echo $pedido['id_factura']; ?>" class="btn btn-success">PDF</a>
 
                                 <form method="post" style="display: inline;">
                                     <input type="hidden" name="id_pedido" value="<?php echo $pedido['id_factura']; ?>">
@@ -260,6 +259,7 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 </body>
 
 </html>
