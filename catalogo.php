8d0e74828f444ca0cc106b8526d5cdb40cce7bfe
NicoAvella09 <140751616+NicoAvella09@users.noreply.github.com>
2024-05-31 16:51:42 -0500

    Update catalogo.php

--- catalogo.php
+++ catalogo.php
@@ -318,7 +318,7 @@
 
                         <img class="img-item" src="<?php echo $row['imagen']; ?>" alt="<?php echo $row['categoria_producto']; ?>">
 
-                        <h5>*EL PRECIO DE VENTA ES POR CANASTILLA*</h5>
+                        <h5>*EL PRECIO DE VENTA ES POR CANASTILLAS*</h5>
                         <h6>UNA CANASTILLA = "8KG"</h6>
                         <span class="precio-item">$<?php echo $row['precio_producto']; ?></span>
 
