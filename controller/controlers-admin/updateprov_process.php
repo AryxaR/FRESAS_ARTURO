a0b8628cc8b1cbad80c5e8cbfde1a86cc3f4ff38
NicoAvella09 <140751616+NicoAvella09@users.noreply.github.com>
2024-06-26 13:21:54 -0500

    ola 2

--- controller/controlers-admin/updateprov_process.php
+++ controller/controlers-admin/updateprov_process.php
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $sql_update_recurso = "UPDATE recursos SET Stock = $stock WHERE Id_proveedor = $id";
             if ($conn->query($sql_update_recurso) === TRUE) {
                 $msj_exito = "Actualizar";
-                header("Location: ../../model/interfaz_admin/update_proveedor.php?msj_exito= $msj_exito");
+                header("Location: ../../model/interfaz_admin/Proveedores.php?msj_exito= $msj_exito");
             } else {
                 echo "Error al actualizar datos en la tabla Recursos: " . $conn->error;
             }
