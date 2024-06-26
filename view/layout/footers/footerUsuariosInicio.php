42fdce0ddaa0d12f89e8f8808a0691f2d44901fd
NicoAvella09 <140751616+NicoAvella09@users.noreply.github.com>
2024-06-26 07:05:38 -0500

    footer index

--- view/layout/footers/footerUsuariosInicio.php
+++ view/layout/footers/footerUsuariosInicio.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../../../../FRESAS_ARTURO//resource/css/footer-usuarios.css">
+    <link rel="stylesheet" href="../../../resource/css/footer-usuarios.css">
 </head>
 <body>
 <footer class="footer">
@@ -17,7 +17,7 @@
     <div class="footer-links">
         <h2>Enlaces útiles</h2>
         <ul>
-            <li><a href="../FRESAS_ARTURO/index.php">Inicio</a></li>
+            <li><a href="../../../index.php">Inicio</a></li>
             <li><a href="#section-carrito">Catálogo</a></li>
             <li><a href="#section-contacto">Contacto</a></li>
             <!-- <li><a href="#">Manual de usuraio</a></li> -->
@@ -26,8 +26,8 @@
     <div class="footer-ingreso">
         <h2>Inicia sesion o registrate</h2>
         <ul>
-            <li><a href="../../../../FRESAS_ARTURO/model/login_usuarios.php">Ingresar</a></li>
-            <li><a href="../../../../FRESAS_ARTURO/model/login_usuarios.php?click">Registro</a></li>
+            <li><a href="../../../model/login_usuarios.php">Ingresar</a></li>
+            <li><a href="../../../model/login_usuarios.php?click">Registro</a></li>
         </ul>
     </div>
     <div class="footer-copyright">
